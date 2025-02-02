ea4adfbd567dd71244c5675c6ab4604f5ba17a33
QRECUERDOBO <qrecuerdobol@gmail.com>
2025-02-02 01:59:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,24 +2,20 @@
 const musicButton = document.getElementById('music-button');
 const backgroundMusic = document.getElementById('background-music');
 
-// Reproducir música al hacer clic en el botón
 musicButton.addEventListener('click', () => {
   if (backgroundMusic.paused) {
     backgroundMusic.play();
-    musicButton.textContent = '⏸️ Pausar música';
+    musicButton.textContent = '⏸️ Pausar';
   } else {
     backgroundMusic.pause();
-    musicButton.textContent = '▶️ Reproducir música';
+    musicButton.textContent = '▶️ Reproducir';
   }
 });
 
 // Animación del carrusel
 const carouselInner = document.querySelector('.carousel-inner');
-
-// Clona las imágenes para crear un efecto de bucle infinito
 carouselInner.innerHTML += carouselInner.innerHTML;
 
-// Ajusta la duración de la animación según el número de imágenes
 const images = document.querySelectorAll('.carousel img');
 const totalWidth = images.length * images[0].clientWidth;
-carouselInner.style.width = ${totalWidth}px;
+carouselInner.style.width = `${totalWidth}px`;
